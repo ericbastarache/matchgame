0f1108e63442cc52d237426e2a3a7beb35b5f8df
Eric Bastarache <eric_bastarache@hotmail.com>
2017-06-11 00:31:34 -0400

    Removed code from board and tile components for refactoring purposes

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './Tile.css';
 
-class Tile extends Component {
-  render () {
-    return (
-      <h1>Test</h1>
-    )
-  }
+const Tile = () => {
+  return (
+    
+  )
 }
 
 export default Tile;
