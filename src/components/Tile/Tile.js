9359996020b0ee1bba0d70e238eb6f48c6be010f
Eric Bastarache <eric_bastarache@hotmail.com>
2017-06-11 00:05:59 -0400

    Added preliminary markup for tile component

---
+++ src/components/Tile/Tile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Tile.css';
+
+class Tile extends Component {
+  render () {
+    return (
+      <h1>Test</h1>
+    )
+  }
+}
+
+export default Tile;
