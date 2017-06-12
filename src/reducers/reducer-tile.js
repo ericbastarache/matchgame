73e13e4cf36e3b7d5d091db34ebc1a565beb5dbd
Eric Bastarache <eric_bastarache@hotmail.com>
2017-06-12 01:30:25 -0400

    Updated tile reducer

--- src/reducers/reducer-tile.js
+++ src/reducers/reducer-tile.js
@@ -2,12 +2,15 @@ const INITIAL_STATE = [];
 const SET_TILE = 'SET_TILE';
 const NEW_GAME = 'NEW_GAME';
 
-export default (state = INITIAL_STATE, action) => {
+export default function (state = INITIAL_STATE, action) {
   switch(action.type) {
     case SET_TILE:
       return action.tile;
 
     case NEW_GAME:
+      return INITIAL_STATE;
+
+    default:
       return INITIAL_STATE;
   }
   return state;
