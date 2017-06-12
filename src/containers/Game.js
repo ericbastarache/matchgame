0979257901ca315d79d7d06f7a99f8cd25100b70
Eric Bastarache <eric_bastarache@hotmail.com>
2017-06-12 01:30:31 -0400

    Updated game component

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import Board from '../components/Board/Board';
 import Tile from '../components/Tile/Tile';
 
@@ -16,10 +17,36 @@ class Game extends Component {
   componentDidUpdate () {
     this.checkClicked();
   }
+
+  checkClicked = () => {
+
+  }
+
+  setClickable () {
+
+  }
+
+  render () {
+    return (
+      <div>
+        <h1 className="text-right">Score: { this.props.game.score }</h1>
+        <h3 className="text-right">High Score: { this.props.game.highScore }</h3>
+      </div>
+    )
+  }
 }
 
 mapStateToProps = (props) => {
   return props;
 }
 
-export default connect(mapStateToProps)(Game);
+mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    setActiveTile,
+    newGame,
+    gameOver,
+    incrementScore
+  }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Game);
