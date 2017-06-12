8f1d8e05d76d8f60d132b3e6828884a912d967a0
Eric Bastarache <eric_bastarache@hotmail.com>
2017-06-12 01:34:01 -0400

    Created initial actions

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+//Game Reducer actions
+const LOST_GAME = 'LOST_GAME';
+const NEW_GAME = 'NEW_GAME';
+const INCREMENT_SCORE = 'INCREMENT_SCORE';
+
+//Tile Reducer actions
+const SET_TILE = 'SET_TILE';
