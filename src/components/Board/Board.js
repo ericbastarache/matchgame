3107b1a6f0501f1387d99c73adaa010afee19bc8
Eric Bastarache <eric_bastarache@hotmail.com>
2017-06-12 00:07:44 -0400

    Updated board component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import Tile from '../Tile/Tile';
 import './Board.css';
 
-const Board = () => {
-
+const Board = (props) => {
+  return (
+    <div>
+      {
+        props.boardsize.map((size, index) => {
+          return <Tile number={size} key={index} />
+        })
+      }
+    </div>
+  )
 }
 
 export default Board;
