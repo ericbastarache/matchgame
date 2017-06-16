d860d5066c09912ca149b4c5fcc4c4b955c106de
Eric Bastarache <eric_bastarache@hotmail.com>
2017-06-15 23:13:36 -0400

    Updated game container

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Board from '../components/Board/Board';
-import Tile from '../components/Tile/Tile';
+import { newGame, getActiveTile, setActiveTile, gameOver, incrementScore } from '../actions';
 
 class Game extends Component {
   constructor () {
     super ();
-    this.boardsize = 5;
+    this.boardsize = 9;
   }
 
   componentWillMount () {
@@ -15,34 +15,47 @@ class Game extends Component {
   }
 
   componentDidUpdate () {
+    this.startGame();
     this.checkClicked();
   }
 
-  checkClicked = () => {
-
+  checkClicked () {
+    this.props.getActiveTile();
   }
 
   setClickable () {
+    //this.props.setActiveTile();
+  }
 
+  startGame = () => {
+    console.log(this.props);
+    if(this.props.game.lost) return false;
+    clearInterval(this.tileInterval);
+    this.tileInterval = setInterval(() => {
+      this.props.setActiveTile(this.props.tilenumber);
+    }, 2000);
   }
 
   render () {
     return (
       <div>
-        <h1 className="text-right">Score: { this.props.game.score }</h1>
-        <h3 className="text-right">High Score: { this.props.game.highScore }</h3>
+        <button className="start-button" onClick={this.startGame}>Start Game!</button>
+        <h1 className="text-center">Score: { this.props.game.score }</h1>
+        <h3 className="text-center">High Score: { this.props.game.highScore }</h3>
+        <Board />
       </div>
     )
   }
 }
 
-mapStateToProps = (props) => {
+const mapStateToProps = (props) => {
   return props;
 }
 
-mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
     setActiveTile,
+    getActiveTile,
     newGame,
     gameOver,
     incrementScore
