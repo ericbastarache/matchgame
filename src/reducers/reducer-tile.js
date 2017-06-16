83c7b9c1541a0143bb164c5d7bdbea2a8b83af31
Eric Bastarache <eric_bastarache@hotmail.com>
2017-06-15 23:14:01 -0400

    Updated tile reducer

--- src/reducers/reducer-tile.js
+++ src/reducers/reducer-tile.js
@@ -1,11 +1,28 @@
-import { SET_TILE, NEW_GAME } from '../actions';
+import { GET_TILE, SET_TILE, NEW_GAME } from '../actions';
 
-const INITIAL_STATE = [];
+const INITIAL_STATE = {
+  tile: 0
+}
+
+const updateClickable = ({tile}) => {
+  return Math.floor(Math.random() * 9) + 1;
+}
+
+const getTile = ({tile}) => tile;
 
 export default function (state = INITIAL_STATE, action) {
   switch(action.type) {
     case SET_TILE:
-      return action.tile;
+      return {
+        ...state,
+        tile: updateClickable(state)
+      }
+
+    case GET_TILE:
+      return {
+        ...state,
+        tile: getTile(state)
+      }
 
     case NEW_GAME:
       return INITIAL_STATE;
@@ -13,5 +30,4 @@ export default function (state = INITIAL_STATE, action) {
     default:
       return INITIAL_STATE;
   }
-  return state;
 }
