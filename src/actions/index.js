40d3877b068521f6b802ce9d378237f54f457d1d
Eric Bastarache <eric_bastarache@hotmail.com>
2017-06-15 23:13:02 -0400

    Updated actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,38 @@
 //Game Reducer actions
-const LOST_GAME = 'LOST_GAME';
-const NEW_GAME = 'NEW_GAME';
-const INCREMENT_SCORE = 'INCREMENT_SCORE';
+export const LOST_GAME = 'LOST_GAME';
+export const NEW_GAME = 'NEW_GAME';
+export const INCREMENT_SCORE = 'INCREMENT_SCORE';
 
 //Tile Reducer actions
-const SET_TILE = 'SET_TILE';
+export const SET_TILE = 'SET_TILE';
+export const GET_TILE = 'GET_TILE';
+
+export const setActiveTile = () => {
+  return {
+    type: SET_TILE
+  }
+}
+
+export const getActiveTile = () => {
+  return {
+    type: GET_TILE
+  }
+}
+
+export const newGame = () => {
+  return {
+    type: NEW_GAME
+  }
+}
+
+export const gameOver = () => {
+  return {
+    type: LOST_GAME
+  }
+}
+
+export const incrementScore = () => {
+  return {
+    type: INCREMENT_SCORE
+  }
+}
